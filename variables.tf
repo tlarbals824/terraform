43b920aa2a81038061342346300bfb0bf6f02d80
tlarbals824 <simkm0824@naver.com>
2026-01-03 22:13:43 +0900

    fix: update kubernetes version to v1.31.1 (v1.30.1 no longer supported)

--- variables.tf
+++ variables.tf
@@ -51,5 +51,5 @@ variable "common_tags" {
 variable "kubernetes_version" {
   description = "Kubernetes version for OKE cluster"
   type        = string
-  default     = "v1.30.1"
+  default     = "v1.31.1"
 }
