959bbf68c536a5a6363418371313406a574254c3
tlarbals824 <simkm0824@naver.com>
2026-01-04 11:27:27 +0900

    fix: remove AMD node pool (not supported by OKE)

--- terraform/data.tf
+++ terraform/data.tf
@@ -18,12 +18,3 @@ data "oci_core_images" "arm" {
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
 }
-
-data "oci_core_images" "amd" {
-  compartment_id           = var.compartment_ocid
-  operating_system         = "Oracle Linux"
-  operating_system_version = "8"
-  shape                    = "VM.Standard.E2.1.Micro"
-  sort_by                  = "TIMECREATED"
-  sort_order               = "DESC"
-}
